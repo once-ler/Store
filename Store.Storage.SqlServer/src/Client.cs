284fc3bd935fc3eba81dab8fa8078336144c0e98
once-ler <htao@eztier.com>
2017-11-15 15:11:55 -0500

    Update Store.SqlServer connection string constructor

--- Store.Storage.SqlServer/src/Client.cs
+++ Store.Storage.SqlServer/src/Client.cs
@@ -9,16 +9,17 @@ namespace Store.Storage.SqlServer {
   public class Client {
     public Client(DBContext _dbContext) {
       var fac = new Factory<SqlConnection>();
-      dbConnection = fac.Get(_dbContext);
+      Func<DBContext, string> func = (DBContext _db) => _db.userId.Length == 0 || _db.password.Length == 0 ? string.Format("Data Source={0};Initial Catalog={1};Integrated Security=SSPI;", _db.server, _db.database) : string.Format("Data Source={0};Initial Catalog={1};User ID={2};Password={3};", _db.server, _db.database, _db.userId, _db.password);
+      dbConnection = fac.Get(_dbContext, func);
     }
 
     protected string tryCatch(Action act) { try { act(); } catch (Exception e) { return e.Message; } return OperatonResult.Succeeded.ToString("F"); }
 
-    internal string runSql(string sql) {
+    public string runSql(string sql) {
       return tryCatch(() => { var runner = new CommandRunner(dbConnection); runner.Transact(new DbCommand[] { runner.BuildCommand(sql, null) }); });
     }
 
-    internal IEnumerable<dynamic> runSqlDynamic(string sql) {
+    public IEnumerable<dynamic> runSqlDynamic(string sql) {
       var runner = new CommandRunner(dbConnection); return runner.ExecuteDynamic(sql, null);
     }
 
