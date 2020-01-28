7f0a620b6473d4df6193a30a356e879e9929dc92
once-ler <htao@eztier.com>
2020-01-28 15:46:17 -0500

    First version of export to excel from ienumerable

---
+++ Store.Reports.Formats/src/Excel.fs
@@ -0,0 +1,50 @@
+﻿namespace Store.Reports.Formats
+
+open NPOI.XSSF.UserModel
+open System
+open System.Collections.Generic
+open System.Collections.Generic.lin
+open Newtonsoft.Json
+open System.IO
+open System.Data
+
+type Excel() =
+
+  member private this.generateHeader(dt: DataTable, sheet: NPOI.SS.UserModel.ISheet) =
+    let headerRow = sheet.CreateRow(0)
+    for col in dt.Columns do
+      let c = headerRow.CreateCell(col.Ordinal)
+      c.SetCellValue(col.ColumnName.ToUpper())
+
+  member this.export<'A> (data: IEnumerable<'A>, fileName: string, ?maybeSheetName: string): MemoryStream =
+    let sheetName =
+      match maybeSheetName with
+        | a when a.IsSome -> maybeSheetName.Value
+        | _ -> "Sheet1"
+
+    let wb = new XSSFWorkbook()
+    let sheet = wb.CreateSheet(sheetName)
+    let headerStyle = wb.CreateCellStyle()
+    let headerFont = wb.CreateFont();
+    headerFont.IsBold <- true;
+    headerStyle.SetFont(headerFont)
+
+    let sheet = wb.CreateSheet(sheetName)
+    
+    let j = JsonConvert.SerializeObject(data)
+    let dt: DataTable = JsonConvert.DeserializeObject<DataTable>(j)
+
+    this.generateHeader(dt, sheet)
+
+    for i in 0 .. dt.Rows.Count do
+      let row = dt.Rows.[i]
+      let r = sheet.CreateRow(i + 1)
+      for col in dt.Columns do
+        let c = r.CreateCell(col.Ordinal)
+        let v = Convert.ToString(row.[col.Ordinal])
+        c.SetCellValue(v)
+
+    use stream = new MemoryStream()
+    wb.Write(stream)
+
+    stream
