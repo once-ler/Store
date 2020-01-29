de69e6af476a24d2e84180e0ec9f1d397d3e41c3
once-ler <htao@eztier.com>
2020-01-29 11:21:56 -0500

    Properly set FAKE v4 and build.fsx

--- Store.Reports.Formats/build.fsx
+++ Store.Reports.Formats/build.fsx
@@ -1,23 +1,70 @@
-﻿open System
-open System.IO
-open Fake.Core
-open Fake.Git
-open Fake.IO
-open Fake.IO.FileSystemOperators
-open Fake.IO.Globbing
-open Fake.IO.Globbing.Operators
-open Fake.DotNet
+﻿// include Fake libs
+// https://stackoverflow.com/questions/39726728/f-unit-test-projects-in-linux-with-mono-fake-nunit-3
+// https://stackoverflow.com/questions/41815649/f-how-to-setup-fake-project-that-can-use-fsunit
+// chmod a+x ../packages/FAKE.4.64.17/tools/FAKE.exe
+// ../packages/FAKE.4.64.17/tools/FAKE.exe build.fsx
+#r "../packages/FAKE.4.64.17/tools/FakeLib.dll"
+
+open System
+open Fake
+open Fake.Testing // NUnit3 is in here
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+// RestorePackages()
+
+// Directories
+let buildDir  = "./build/"
+let testDir = "./output/"
+
+// version info
+let version = "0.1"  // or retrieve from CI server
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; testDir]
+)
+
+Target "Build" (fun _ ->
+    //MSBuildDebug buildDir "Build" appReferences
+    // !! "/UnitTesting/*.fsproj"
+    !! "/*.fsproj"
+    |> MSBuildRelease buildDir "Build"
+    |> Log "AppBuild-Output: "
+)
+
+(*
+Target "BuildTest" (fun _ ->
+                    !! "test/NUnit.Test.MyTests/*.fsproj"
+                    |> MSBuildDebug testDir "Build"
+                    |> Log "TestBuild-Output: "
+)
+*)
+
+Target "Test" (fun _ ->
+               !! (testDir + "/Store.Reports.Formats.Specs.dll")
+               |> NUnit3 (fun p ->
+                         { p with
+                               ToolPath = "../packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe"
+                               // DisableShadowCopy = true;
+                               // OutputFile = testDir + "TestResults.xml" 
+                               })
+)
 
 let files includes = 
   { BaseDirectory = __SOURCE_DIRECTORY__
     Includes = includes
     Excludes = [] }
 
-Target "Test" (fun _ ->
-    !! "/tests*.dll |> NUnit (fun p ->
+Target "Test2" (fun _ ->
+    !! "/tests*.dll" |> NUnit (fun p ->
         {p with
            DisableShadowCopy = true;
            OutputFile = testDir + "TestResults.xml" })
 )
+
+Target "Default" (fun _ -> trace "Completed FAKE!!!")
+
+"Clean" ==> "Build" ==> "Test" ==> "Default"
+
+RunTargetOrDefault "Default"
