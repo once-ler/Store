ab8b750025cf8e953446c03150a981fe42b6c2c7
once-ler <htao@eztier.com>
2020-01-28 19:42:10 -0500

    Add FAKE runner

---
+++ Store.Reports.Formats/build.fsx
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+open Fake.Core
+open Fake.Git
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing
+open Fake.IO.Globbing.Operators
+open Fake.DotNet
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let files includes = 
+  { BaseDirectory = __SOURCE_DIRECTORY__
+    Includes = includes
+    Excludes = [] }
+
+Target "Test" (fun _ ->
+    !! "/tests*.dll |> NUnit (fun p ->
+        {p with
+           DisableShadowCopy = true;
+           OutputFile = testDir + "TestResults.xml" })
+)
