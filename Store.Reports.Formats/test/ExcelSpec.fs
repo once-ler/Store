7d67556bb84e5cab75d0424c61b3d6326da55942
once-ler <htao@eztier.com>
2020-01-28 19:15:54 -0500

    Add Interop.Dynamic

--- Store.Reports.Formats/test/ExcelSpec.fs
+++ Store.Reports.Formats/test/ExcelSpec.fs
@@ -1,5 +1,28 @@
-﻿namespace Store.Reports.Formats.Test
+﻿namespace Store.Reports.Formats.Specs
 
 open NUnit.Framework
 open FsUnit
 
+open System.Dynamic
+open FSharp.Interop.Dynamic
+
+module ``Excel Tests`` =
+
+  open Store.Reports.Formats
+
+  let listA = [ 
+    for i in 0 .. 9 do
+      let o = new ExpandoObject()
+      o?id <- i
+      o?name <- "ABC"
+      yield o
+    ]
+
+  [<Test>]
+  let ``When List of objects``([<Values()>] input) =
+
+    let excel = new Excel()
+
+    let res = excel.export(listA, "file-a")
+
+    1 |> should equal 1
